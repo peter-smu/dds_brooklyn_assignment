19f037d188a9b7a9e2cc9e63f45a77485d6245f3
ht_peter <peter@hoosthereapp.com>
2017-03-02 07:30:53 -0500

    cleaned README.md + organized Heidi's files

--- Analysis/staten_analysis.R
+++ Analysis/staten_analysis.R
@@ -1,39 +1,11 @@
-#uploading data
-staten_data <- read.csv("./Data/rollingsales_statenisland.csv")
-# Author: Benjamin Reddy
-# Taken from pages 49-50 of O'Neil and Schutt
+############################################
+## Asha Mohan, Peter Kouvaris, Heidi Nguyen.
+## MSDS 6306 
+## Assignment Unit 6 
+############################################
 
-#require(gdata)
-#require(plyr) #Added by Monnie McGee
-#install the gdata and plyr packages and load in to R.
-library(plyr)
-library(gdata)
-
-
-
-
-# So, save the file as a csv and use read.csv instead
-staten <- read.csv("./Data/rollingsales_statenisland.csv")
-
-## Check the data
-head(staten)
-summary(staten)
-str(staten) # Very handy function!
-
-## clean/format the data with regular expressions
-## More on these later. For now, know that the
-## pattern "[^[:digit:]]" refers to members of the variable name that
-## start with digits. We use the gsub command to replace them with a blank space.
-# We create a new variable that is a "clean' version of sale.price.
-# And sale.price.n is numeric, not a factor.
-staten$SALE.PRICE.N <- as.numeric(gsub("[^[:digit:]]","", staten$SALE.PRICE))
-count(is.na(staten$SALE.PRICE.N))
-
-names(staten) <- tolower(names(staten)) # make all variable names lower case
-## Get rid of leading digits
-staten$gross.sqft <- as.numeric(gsub("[^[:digit:]]","", staten$gross.square.feet))
-staten$land.sqft <- as.numeric(gsub("[^[:digit:]]","", staten$land.square.feet))
-staten$year.built <- as.numeric(as.character(staten$year.built))
+# use read.csv to import the cleaned data set
+staten <- read.csv("./Data/clean_staten.csv")
 
 ## do a bit of exploration to make sure there's not anything
 ## weird going on with sale prices
@@ -41,20 +13,36 @@ attach(staten)
 hist(sale.price.n) 
 detach(staten)
 
-## keep only the actual sales
-
+## keep only the actual sales where price is not $0 
 staten.sale <- staten[staten$sale.price.n!=0,]
-plot(staten.sale$gross.sqft,staten.sale$sale.price.n)
-plot(log10(staten.sale$gross.sqft),log10(staten.sale$sale.price.n))
+plot(staten.sale$gross.sqft,staten.sale$sale.price.n,main="Fig.1: Sales Price vs. Gross Sqft")
+plot(log10(staten.sale$gross.sqft),log10(staten.sale$sale.price.n),main="Fig.2: Log(Sales Price) vs. Log(Gross Sqft)")
 
-## for now, let's look at 1-, 2-, and 3-family homes
+## Look at 1-, 2-, and 3-family homes
 staten.homes <- staten.sale[which(grepl("FAMILY",staten.sale$building.class.category)),]
+## Number of 1-, 2-, and 3-family homes 
 dim(staten.homes)
-plot(log10(staten.homes$gross.sqft),log10(staten.homes$sale.price.n))
+## Plot of sales price vs. land square footage.
+plot(log10(staten.homes$gross.sqft),log10(staten.homes$sale.price.n),main="Fig.3: log(Sales Price) vs. log(Gross Sqft) for 1 to 3 \n Family Dwelling")
+## Explore those homes with sale prices < 100000, which look like outliner.  
 summary(staten.homes[which(staten.homes$sale.price.n<100000),])
+## Explore homes with sqft < 100 ft
+summary(staten.homes[which(staten.homes$gross.sqft<100),])
 
 
-## remove outliers that seem like they weren't actual sales
-staten.homes$outliers <- (log10(staten.homes$sale.price.n) <=5) + 0
+## remove outliers that seem like they weren't actual sales (WITH PRICE <= $100000 AND SQFT < 100)
+staten.homes$outliers <- (log10(staten.homes$sale.price.n) <=5) + (log10(staten.homes$gross.sqft) <=2)
 staten.homes <- staten.homes[which(staten.homes$outliers==0),]
-plot(log10(staten.homes$gross.sqft),log10(staten.homes$sale.price.n))
+plot(log10(staten.homes$gross.sqft),log10(staten.homes$sale.price.n),main="Fig.4: log(Sales Price) vs. log(Gross Sqft) for 1 to 3 \n Family Dwelling (>$100K and > 100 gross sqft)")
+
+## summary of the data set after removing outliners. 
+summary(staten.homes)
+
+## It appears that there's a positive correlation between price and gross sqft
+## larger homes sold for higher price. 
+## correlation is r = 0.55870
+cor(log10(staten.homes$gross.sqft),log10(staten.homes$sale.price.n))
+
+## linear regression line of 
+plot(log10(staten.homes$gross.sqft) ~ log10(staten.homes$sale.price.n), main="Fig.5: Regression of log(Sales Price) ~ log(Gross Sqft) for 1 to 3 \n Family Dwelling (>$100K and > 100 gross sqft)")
+abline(lm(log10(staten.homes$gross.sqft) ~ log10(staten.homes$sale.price.n)), col="red")
--- README.md
+++ README.md
@@ -1,45 +1,30 @@
 # dds_brooklyn_assignment
 DDS Assignment for 2017.03.02
 
-Synopsis
+###Synopsis###
 
-    This project is to record and analyse the rolling sales of properties in staten island.
-    The data includes different kind of properties from one, two family houses, condos, 
-    vacant land, commercial units etc. Details on square feet, address, tax, sale price and data are available for analysis. 
+    This project is to record and analyze the rolling sales of properties in staten island.
+    The data includes different kind of properties from one, two family houses, condos,
+    vacant land, commercial units etc. Details on square feet, address, tax, sale price and data are available for analysis.
 
-Code Example
+###Structure of project###
 
-Read the csv formatted data
+  The project consists of 3 major pieces:
+    1. Data - raw data files pulled from [NYC Gov](http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page) cleaned for Analysis.
+    2. Analysis - cleaned data was summarized and meaningful relationships were visualized.
+    3. Paper - the process was compiled into a readable format for review.
 
-read.csv("Data/rollingsales_statenisland.csv", header = TRUE, sep = ",")
+###General###
 
-Clean some of the fields in the data
+  The project is a first project done to understand how to interact with GitHub, R Studio and push and pull files between the two. Students were expected to install R Studio and create account in GitHub. Link their GitHub account to R Studio and push to a shared repository to complete the above work.
 
-statenisland_data$YEAR.BUILT <- as.numeric(as.character(statenisland_data$YEAR.BUILT))
-statenisland_data$SALE.PRICE <- as.numeric(gsub("[^[:digit:]]", "", statenisland_data$SALE.PRICE))
+  Tests run under R-Studio.
 
-Plot histograms.
+###Contributors###
 
-Motivation
-
-The project is a first project done to understand how to interact with GitHub, R Studio and push and pull files between the two. 
-
-Installation
-
-Install R Studio and create account in GitHub
-
-API Reference
-
-Not used
-
-Tests
-Run under R-Studio.
-
-Contributors
-
-Peter Kouvaris
-Asha Mohan
-Heidi Nguyen
+  Peter Kouvaris
+  Asha Mohan
+  Heidi Nguyen
 
 License
 Opensource
